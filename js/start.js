4d6bdffc00b8339a4ddb268ca3292b0aba2e08bb
Rashi Sharma <rashi141096@gmail.com>
2017-08-18 00:36:42 +0530

    Add files via upload

---
+++ js/start.js
@@ -0,0 +1,23 @@
+function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("wrapper").style.visibility = "hidden";
+    }
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("wrapper").style.visibility = "visible";
+	}
+var map;
+    // Constructor creates a new map
+    function initMap() {
+      	geocoder = new google.maps.Geocoder();
+      	map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: 26.8467, lng: 80.9462},
+                zoom: 13,
+                mapTypeControl: false
+              });
+    	}
+    function error(){
+        alert('There was an error with the Google API call.'+
+              'Please refresh the page and try again');
+    }
